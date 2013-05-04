e7d334a17b6cacdd091e8e4ce591b3489e416fc0
Sepher Liu <sepher.liu@gmail.com>
2013-05-05 03:15:20 +0800

    use new google analytics code

--- widgets.go
+++ widgets.go
@@ -18,10 +18,14 @@ var (
 const (
 	Analytics_google = `
 <script>
-    var _gaq=[['_setAccount','%s'],['_trackPageview']];
-    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-    s.parentNode.insertBefore(g,s)}(document,'script'));
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', '%s', 'github.io');
+  ga('send', 'pageview');
+
 </script>`
 	Comments_disqus = `
 <div id="disqus_thread"></div>
