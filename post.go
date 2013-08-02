58b6034c7a9c7da7706eda18c002639d1e2e370c
靖天 <ifqqfi@gmail.com>
2013-08-02 22:21:38 +0800

    添加img 配置文件支持。

--- post.go
+++ post.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"strings"
 	"time"
+	"path/filepath"
 )
 
 const (
@@ -22,8 +23,9 @@ tags:
 ---
 
 `
-	IMG_TAG = `<img src="%s" alt="img: " width="600" class="imgbox">`
-	IMG_LOCATION = `http://iwood.qiniudn.com/`
+	IMG_TAG = `<img src="%s" alt="img: " width="600">`
+	IMG_URLPERFIX = `{{urls.media}}/`
+	IMG_LOCALDIR = `media/`
 )
 
 // 创建一个新post
@@ -48,6 +50,10 @@ func CreateNewPost(title string) (path string){
 
 func CreateNewPostWithImgs(title, imgsrc string) {
 
+	cfg := loadConfig(".")
+	for k, v := range cfg {
+		log.Println(k, "=", v)
+	}
 	path := CreateNewPost(title)
 
 	start := strings.LastIndex(path, "/") + 1
@@ -58,8 +64,8 @@ func CreateNewPostWithImgs(title, imgsrc string) {
 	post := path[start:end]
 
 	// 如果创建失败直接exit，所以不用检查
-	imgs := cpPostImgs(post, imgsrc)
-	tags := generateImgLinks(imgs)
+	imgs := cpPostImgs(post, imgsrc, cfg)
+	tags := generateImgLinks(imgs, cfg)
 
 	f, err := os.OpenFile(path, os.O_APPEND|os.O_WRONLY, 0600)
 	if err != nil {
@@ -74,7 +80,7 @@ func CreateNewPostWithImgs(title, imgsrc string) {
 	}
 }
 
-func cpPostImgs(post, imgsrc string) (imgtag []string) {
+func cpPostImgs(post string, imgsrc string, cfg Mapper) (imgtag []string) {
 	files, err := ioutil.ReadDir(imgsrc)
 	if files == nil || err != nil {
 		log.Println("no img file exists.");
@@ -85,7 +91,7 @@ func cpPostImgs(post, imgsrc string) (imgtag []string) {
 		imgsrc += "/"
 	}
 
-	imgdst := "rc/media/" + post
+	imgdst := cfg.GetString("localdir") + post
 	_, err = os.Stat(imgdst)
  	if os.IsNotExist(err) {
 		os.MkdirAll(imgdst, 0777)
@@ -100,7 +106,7 @@ func cpPostImgs(post, imgsrc string) (imgtag []string) {
 			continue
 		}
 		//log.Println(idx, imgdst + "/" + f.Name(), imgsrc + f.Name())
-		imgtag[i] = imgdst + "/" + f.Name()
+		imgtag[i] = post + "/" + f.Name()
 		i++
 	}
 	imgtag = imgtag[:i]
@@ -126,14 +132,51 @@ func cp(dst, src string) error {
 	return d.Close()
 }
 
-func generateImgLinks(files []string) (links []string){
+func generateImgLinks(files []string, cfg Mapper) (links []string){
 	links = make([]string, len(files))
 	for i, f := range files {
-		tmp := strings.TrimLeft(f, "rc/")
-		links[i] = fmt.Sprintf(IMG_TAG, IMG_LOCATION + tmp)
+		//tmp := strings.TrimLeft(f, "rc/")
+		links[i] = fmt.Sprintf(cfg.GetString("imgtag"), cfg.GetString("urlperfix") + f)
 		println(i, links[i])
 	}
 		
+	return
+}
+
+func loadConfig(root string) (imgs_cfg Mapper) {
+	var cfg Mapper
+	var err error
+
+	if root == "" {
+		root = "."
+	}
+	root, err = filepath.Abs(root)
+	root += "/"
+	log.Println("root=", root)
+
+	cfg, err = ReadYml(root + CONFIG_YAML)
+	if err != nil {
+		log.Println("Fail to read ", root+CONFIG_YAML, err)
+		return
+	}
+
+	
+	if cfg["imgs"] == nil {
+		imgs_cfg = make(Mapper)
+	} else {
+		imgs_cfg = cfg["imgs"].(map[string]interface {})
+	}
+
+	if imgs_cfg["imgtag"] == nil {
+		imgs_cfg["imgtag"] = IMG_TAG
+	}
+	if imgs_cfg["urlperfix"] == nil {
+		imgs_cfg["urlperfix"] = IMG_URLPERFIX
+	}
+	if imgs_cfg["localdir"] == nil {
+		imgs_cfg["localdir"] = IMG_LOCALDIR
+	}
+
 	return
 }
 
